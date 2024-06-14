ea62c5872d426952fc5df44af1cca2cdab2b4a53
JawadSadiq01 <creatorjawad@gmail.com>
2024-06-14 20:35:00 +0500

    Right heroSection

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,7 +1,9 @@
 import Logo from "./dashBoard/Logo.svg"
 import Hamburger from "./dashBoard/Hamburger.svg"
+import  HeroImage from "./dashBoard/HeroImage.svg"
 
 export {
   Logo,
-  Hamburger
+  Hamburger,
+  HeroImage
 }
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './Dashboard.css'
 import { Header } from '../../components'
+import { HeroImage } from '../../assets/images'
 const Dashboard:React.FC = () => {
   return (
     <div>
       <Header/>
+      <div className="heroSection">
+        <div className="heroLeft">
+
+        </div>
+        <div className="heroRight">
+          <img src={HeroImage} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
